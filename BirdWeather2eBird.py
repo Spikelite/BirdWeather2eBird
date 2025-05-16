e9da2eb23f9d7a7d0421a2e9e2b766790ba47a42
Spikelite <sg@spikelite.com>
2025-05-15 23:06:55 -0700

    Added eBird datafield matrix to comments

--- BirdWeather2eBird.py
+++ BirdWeather2eBird.py
@@ -14,26 +14,27 @@ def main():
             genus = sci[0] if len(sci) > 0 else ""
             species = sci[1] if len(sci) > 1 else ""
             state, country = core.get_location_codes(row["Latitude"], row["Longitude"])
+            # The order of these datapoints are strictly required by eBird's Extended Record Format
             writer.writerow([
-                row["Common Name"].strip(),
-                genus,
-                species,
-                1,
-                config.SPECIES_COMMENTS,
-                row["Station"].strip(),
-                row["Latitude"],
-                row["Longitude"],
-                date,
-                time,
-                state,
-                country,
-                config.PROTOCOL,
-                config.NUM_OBSERVERS,
-                config.DURATION,
-                config.ALL_OBS_REPORTED,
-                config.DISTANCE_COVERED,
-                config.AREA_COVERED,
-                config.CHECKLIST_COMMENTS
+                row["Common Name"].strip(), # Common Name
+                genus,                      # Genus
+                species,                    # Species
+                1,                          # Species Count
+                config.SPECIES_COMMENTS,    # Species Comments
+                row["Station"].strip(),     # Location Name
+                row["Latitude"],            # Latitude
+                row["Longitude"],           # Longitude
+                date,                       # Observation Date
+                time,                       # Start Time
+                state,                      # State
+                country,                    # Country
+                config.PROTOCOL,            # Protocol
+                config.NUM_OBSERVERS,       # Number of Observers
+                config.DURATION,            # Duration
+                config.ALL_OBS_REPORTED,    # All Observations Reported?
+                config.DISTANCE_COVERED,    # Distance Covered
+                config.AREA_COVERED,        # Area Covered
+                config.CHECKLIST_COMMENTS   # Checklist Comments
             ])
 
 if __name__ == "__main__":
