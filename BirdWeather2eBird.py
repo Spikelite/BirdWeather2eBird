1f2c48bdd240b511102b5ebe4adb5879db4647a7
Spikelite <sg@spikelite.com>
2025-05-15 22:59:03 -0700

    Added a lib/core_processing.py file and moved existing functions

--- BirdWeather2eBird.py
+++ BirdWeather2eBird.py
@@ -1,42 +1,7 @@
 import csv
 
-from datetime import datetime
-
 from conf import config as config
-
-def parse_timestamp(ts):
-    """
-    Parses the input timestamp and splits it into a date and time tuple
-
-    Args:
-        ts (str): Timestamp to parse
-    
-    Returns:
-        Tuple containing (date,time)
-    """
-    dt = datetime.strptime(ts[:-6], "%Y-%m-%d %H:%M:%S")
-    date = dt.strftime("%m/%d/%Y")
-    time = dt.strftime("%I:%M %p")
-    return date, time
-
-def get_location_codes(lat, lon):
-    """
-    Gets the state and country codes for the provided latitude (lat) and longitude (lon)
-
-    TODO: Currently this function is hard coded to return hard coded global values, in the future it should be able to
-    dynamically get the location information from the lat/lon
-
-    Args:
-        lat (float): Latitude
-        long (float): Longitude
-
-    Returns:
-        Tuple containing (state_code,country_code), where the code will be a 2-character code or empty
-    """
-    state_code = config.STATE
-    country_code = config.COUNTRY
-
-    return state_code, country_code
+from lib import core_processing as core
 
 def main():
     with open(config.INPUT_CSV, newline="", encoding="utf-8") as infile, \
@@ -44,11 +9,11 @@ def main():
         reader = csv.DictReader(infile)
         writer = csv.writer(outfile, lineterminator="\n")
         for row in reader:
-            date, time = parse_timestamp(row["Timestamp"])
+            date, time = core.parse_timestamp(row["Timestamp"])
             sci = row["Scientific Name"].strip().split()
             genus = sci[0] if len(sci) > 0 else ""
             species = sci[1] if len(sci) > 1 else ""
-            state, country = get_location_codes(row["Latitude"], row["Longitude"])
+            state, country = core.get_location_codes(row["Latitude"], row["Longitude"])
             writer.writerow([
                 row["Common Name"].strip(),
                 genus,
---
+++ lib/core_processing.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from conf import config as config
+
+def parse_timestamp(ts):
+    """
+    Parses the input timestamp and splits it into a date and time tuple
+
+    Args:
+        ts (str): Timestamp to parse
+    
+    Returns:
+        Tuple containing (date,time)
+    """
+    dt = datetime.strptime(ts[:-6], "%Y-%m-%d %H:%M:%S")
+    date = dt.strftime("%m/%d/%Y")
+    time = dt.strftime("%I:%M %p")
+    return date, time
+
+def get_location_codes(lat, lon):
+    """
+    Gets the state and country codes for the provided latitude (lat) and longitude (lon)
+
+    TODO: Currently this function is hard coded to return hard coded global values, in the future it should be able to
+    dynamically get the location information from the lat/lon
+
+    Args:
+        lat (float): Latitude
+        long (float): Longitude
+
+    Returns:
+        Tuple containing (state_code,country_code), where the code will be a 2-character code or empty
+    """
+    state_code = config.STATE
+    country_code = config.COUNTRY
+
+    return state_code, country_code
