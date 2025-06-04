38f8a90c41f8da48c20fb706f0546449833ecd19
Spikelite <sg@spikelite.com>
2025-06-03 18:46:55 -0700

    Code cleanup based on PR feedback, many variables renamed

--- BirdWeather2eBird.py
+++ BirdWeather2eBird.py
@@ -17,7 +17,6 @@ You may not:
 All rights are reserved by the author.
 """
 import csv
-import logging
 import os
 import sys
 from datetime import datetime
@@ -26,10 +25,10 @@ from conf import config
 from lib import core_processing
 from lib import cli_support
 
-args = cli_support.input_argparse()
-logger = cli_support.start_logging(config.log_file_path, args.log_level, config.tool_name)
-
 def main():
+    args = cli_support.input_argparse()
+    logger = cli_support.start_logging(config.log_file_path, args.log_level, config.tool_name)
+
     logger.info('Running BirdWeather2eBird')
     logger.debug(f'Input File: {args.input_file}')
     logger.debug(f'Output File: {args.output_file}')
@@ -52,8 +51,8 @@ def main():
     species_counts = {} # Stats: Dict to track count per species
     total_detections = 0 # Stats: Total count of all detections
     detection_firstlast = {
-        "first_detection_datetime": None,
-        "last_detection_datetime": None
+        "first_detection": None,
+        "last_detection": None
     }
     station_details = {
         "station_name": None,
@@ -74,13 +73,13 @@ def main():
                 logger.debug('Entry outside of provided date filter found, skipping, '
                             f'date was: {date}')
                 continue
-            if (detection_firstlast["first_detection_datetime"] is None or
-            current_datetime < detection_firstlast["first_detection_datetime"]):
-                detection_firstlast["first_detection_datetime"] = current_datetime
+            if (detection_firstlast["first_detection"] is None or
+            current_datetime < detection_firstlast["first_detection"]):
+                detection_firstlast["first_detection"] = current_datetime
 
-            if (detection_firstlast["last_detection_datetime"] is None or
-            current_datetime > detection_firstlast["last_detection_datetime"]):
-                detection_firstlast["last_detection_datetime"] = current_datetime
+            if (detection_firstlast["last_detection"] is None or
+            current_datetime > detection_firstlast["last_detection"]):
+                detection_firstlast["last_detection"] = current_datetime
             if date not in unique_dates:
                 unique_dates.append(date)
             scientific_name = row["Scientific Name"].strip()
@@ -88,7 +87,7 @@ def main():
             scientific_name_split = scientific_name.split()
             genus = scientific_name_split[0] if len(scientific_name_split) > 0 else ""
             species = scientific_name_split[1] if len(scientific_name_split) > 1 else ""
-            if station_details["station_name"] == None:
+            if not station_details["station_name"]:
                 station_details["station_name"] = row["Station"].strip()
                 station_details["latitude"] = row["Latitude"]
                 station_details["longitude"] = row["Longitude"]
@@ -152,24 +151,24 @@ def main():
 
     if args.checklist:
         # Get time blocks:
-        time_block_list = core_processing.split_time_range(detection_firstlast["first_detection_datetime"], 
-                                         detection_firstlast["last_detection_datetime"],
+        time_blocks = core_processing.split_time_range(detection_firstlast["first_detection"], 
+                                         detection_firstlast["last_detection"],
                                          core_processing.parse_time_period(args.checklist))
         # Generate a list of block names
-        block_name_list = ["",""]
-        latitude_list = ["Latitude",""]
-        longitude_list = ["Longitude",""]
-        date_list = ["Date",""]
-        start_time_list = ["Start Time",""]
-        state_list = ["State",""]
-        country_list = ["Country",""]
-        protocol_list = ["Protocol",""]
-        num_obs_list = ["Num Observers",""]
-        duration_list = ["Duration (min)",""]
-        all_obs_reported_list = ["All Obs Reported (Y/N)",""]
-        dist_travel_list = ["Dist Traveled (Miles)",""]
-        area_covered_list = ["Area Covered (Acres)",""]
-        notes_list = ["Notes",""]
+        block_names = ["",""]
+        block_latitudes = ["Latitude",""]
+        block_longitudes = ["Longitude",""]
+        block_dates = ["Date",""]
+        block_start_times = ["Start Time",""]
+        block_states = ["State",""]
+        block_countries = ["Country",""]
+        block_protocols = ["Protocol",""]
+        block_num_obs = ["Num Observers",""]
+        block_durations = ["Duration (min)",""]
+        block_all_obs_reported = ["All Obs Reported (Y/N)",""]
+        block_dist_traveled = ["Dist Traveled (Miles)",""]
+        block_area_covered = ["Area Covered (Acres)",""]
+        block_notes = ["Notes",""]
         if args.comments:
             checklist_comments = args.comments
         else:
@@ -178,28 +177,28 @@ def main():
         checklist_duration = core_processing.get_duration(args.checklist)
         block_count = 0
         # This list will contain all of the different block's species counts, which are also lists
-        block_species_counts_list = []
+        species_counts_by_block = []
         # Iterate over our time blocks so that we create separate checklist entries for each block
-        for date_time_tuple in time_block_list:
+        for date_time_tuple in time_blocks:
             with open(args.input_file, newline="", encoding="utf-8") as infile:
                 reader = csv.DictReader(infile)
                 block_count += 1
                 # Append the station specific information to each time block entry in their respective lists
-                block_name_list.append(f'{station_details["station_name"]}-' \
+                block_names.append(f'{station_details["station_name"]}-' \
                                        f'{core_processing.format_time_block(date_time_tuple)}')
-                latitude_list.append(station_details["latitude"])
-                longitude_list.append(station_details["longitude"])
-                date_list.append(date_time_tuple[0].date().strftime("%m/%d/%Y"))
-                start_time_list.append(date_time_tuple[0].time())
-                state_list.append(station_details["state"])
-                country_list.append(station_details["country"])
-                protocol_list.append(args.protocol)
-                num_obs_list.append("1")
-                duration_list.append(checklist_duration)
-                all_obs_reported_list.append("Y")
-                dist_travel_list.append("")
-                area_covered_list.append("")
-                notes_list.append(checklist_comments)
+                block_latitudes.append(station_details["latitude"])
+                block_longitudes.append(station_details["longitude"])
+                block_dates.append(date_time_tuple[0].date().strftime("%m/%d/%Y"))
+                block_start_times.append(date_time_tuple[0].time())
+                block_states.append(station_details["state"])
+                block_countries.append(station_details["country"])
+                block_protocols.append(args.protocol)
+                block_num_obs.append("1")
+                block_durations.append(checklist_duration)
+                block_all_obs_reported.append("Y")
+                block_dist_traveled.append("")
+                block_area_covered.append("")
+                block_notes.append(checklist_comments)
                 # Because the species count information is a bit more dynamic and is a list of lists, we build and
                 # append it to its list by iterating over each row in the input CSV and filtering to the specific time
                 # block.
@@ -213,30 +212,30 @@ def main():
                         common_name = row["Common Name"].strip()
                         species_tuple = (common_name, scientific_name)
                         block_species_counts[species_tuple] = ((block_species_counts.get(species_tuple) or 0) + 1)
-                block_species_counts_list.append(block_species_counts)
+                species_counts_by_block.append(block_species_counts)
 
         # Write the final output into the eBird Checklist Format, as a .csv
         with open(output_file, "w", newline="", encoding="utf-8") as outfile:
             writer = csv.writer(outfile, lineterminator="\n")
-            writer.writerow([*block_name_list])
-            writer.writerow([*latitude_list])
-            writer.writerow([*longitude_list])
-            writer.writerow([*date_list])
-            writer.writerow([*start_time_list])
-            writer.writerow([*state_list])
-            writer.writerow([*country_list])
-            writer.writerow([*protocol_list])
-            writer.writerow([*num_obs_list])
-            writer.writerow([*duration_list])
-            writer.writerow([*all_obs_reported_list])
-            writer.writerow([*dist_travel_list])
-            writer.writerow([*area_covered_list])
-            writer.writerow([*notes_list])
+            writer.writerow([*block_names])
+            writer.writerow([*block_latitudes])
+            writer.writerow([*block_longitudes])
+            writer.writerow([*block_dates])
+            writer.writerow([*block_start_times])
+            writer.writerow([*block_states])
+            writer.writerow([*block_countries])
+            writer.writerow([*block_protocols])
+            writer.writerow([*block_num_obs])
+            writer.writerow([*block_durations])
+            writer.writerow([*block_all_obs_reported])
+            writer.writerow([*block_dist_traveled])
+            writer.writerow([*block_area_covered])
+            writer.writerow([*block_notes])
             # This unpacks our nested species lists and ensures that they are written properly in the output with their
             # common name and species name (*species_key) as the first two columns of each row, followed by the totals
             # for each time block.
             for species_key in species_counts:
-                row = [block.get(species_key, 0) for block in block_species_counts_list]
+                row = [block.get(species_key, 0) for block in species_counts_by_block]
                 writer.writerow([*species_key, *row])
 
     if args.stats:
